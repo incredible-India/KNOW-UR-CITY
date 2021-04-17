30510e4d852d980a28e11a8bafd08c9d548552ef
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-04-17 19:06:15 +0530

    alert box is ready for showing the error

--- js/index.js
+++ js/index.js
@@ -3,11 +3,51 @@ let Zip = document.getElementsByClassName('pinCODE')[0]; //this is element of in
 let SearchBTN = document.getElementsByClassName('btn-warning')[0]; //search button
 
 
+//function for the error box
 
+function ShowError(message,styleType)
+{
+    
+    getALERT.innerHTML = `${message}`;
+getALERT.setAttribute('style',`display : ${styleType};`)
+
+
+}
 // first we will check internet connection 
 
 if(!navigator.onLine)
 {
-        getALERT.innerHTML ="Please connect to the internet"
-        getALERT.setAttribute('style',"display : block;")
+       ShowError("Please Check Your  Internet Connection !!","block");
 }
+
+
+
+//now check the api
+
+SearchBTN.addEventListener('click',()=>{
+
+    if(!navigator.onLine)
+   {
+       ShowError("Please Check Your  Internet Connection !!","block");
+   }
+
+
+   if(Zip.value.length == 0)
+    {
+        ShowError("Enter the pin code first","block");
+    }
+    else
+    {
+console.log(Zip.value);
+
+    }
+
+
+})
+
+//to hide the alert box
+
+Zip.addEventListener("click",()=>{
+
+    ShowError("none","none")
+})
