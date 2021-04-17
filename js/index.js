50dde0562db5d7ce0152fcd6a7d2bc838cae02a1
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-04-17 21:03:45 +0530

    loader added

--- js/index.js
+++ js/index.js
@@ -14,6 +14,18 @@ getALERT.setAttribute('style',`display : ${styleType};`)
 
 
 }
+
+
+
+//hide the loader once image is loaded
+
+function removeIT()
+{
+  document.getElementsByClassName('loading')[0].style.display = "none";
+}
+
+
+
 // first we will check internet connection 
 
 if(!navigator.onLine)
@@ -27,11 +39,16 @@ if(!navigator.onLine)
 
 SearchBTN.addEventListener('click',()=>{
 
+
+
+
     if(!navigator.onLine)
    {
        ShowError("Please Check Your  Internet Connection !!","block");
+       return;
    }
 
+  
 
    if(Zip.value.length == 0)
     {
@@ -48,19 +65,23 @@ SearchBTN.addEventListener('click',()=>{
 
        .then(finalData =>{
          
-        console.log(finalData);
+        // console.log(finalData);
             //first check zip is correct or not
 
             let i =0;
+
+               
                 document.getElementsByClassName('noOFCITY')[0].innerHTML = `<h1 style="text-align: center;" class="display-6"> ${finalData[0].Message} </h1>`
                 document.getElementsByClassName('tableDATA')[0].innerHTML = ""
+              
+             
                 let y = 0; //for the color
 
                     for(i in finalData[0].PostOffice)
                     {
                         document.getElementsByClassName('tableDATA')[0].innerHTML += `
                 
-                        <table class="table table-${colorARRAY[y]} table-striped">
+                        <table class="table table-${colorARRAY[y]} table-striped" autofocus ">
                         <thead>
                             <tr>
                               <th scope="col">SI NO.</th>
@@ -117,3 +138,7 @@ Zip.addEventListener("click",()=>{
 
     ShowError("none","none")
 })
+
+
+
+
