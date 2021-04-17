f13bc9dd8c4bd1e2183376ad6ef30dfb4552f611
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-04-17 19:33:59 +0530

    api fetched

--- js/index.js
+++ js/index.js
@@ -34,11 +34,68 @@ SearchBTN.addEventListener('click',()=>{
 
    if(Zip.value.length == 0)
     {
-        ShowError("Enter the pin code first","block");
+        ShowError("Enter the valid pin code first","block");
     }
     else
     {
-console.log(Zip.value);
+       fetch(`https://api.postalpincode.in/pincode/${Zip.value}`)
+
+       .then(data =>{
+           return data.json(); //convert in json type
+
+       })
+
+       .then(finalData =>{
+         
+        console.log(finalData);
+            //first check zip is correct or not
+
+            let i =0;
+                document.getElementsByClassName('noOFCITY')[0].innerHTML = `<h1 style="text-align: center;"> ${finalData[0].Message} </h1>`
+
+
+                    for(i in finalData[0].PostOffice)
+                    {
+                        document.getElementsByClassName('tableDATA')[0].innerHTML += `
+                
+                        <table class="table table-success table-striped">
+                        <thead>
+                            <tr>
+                              <th scope="col">SI NO.</th>
+                              <th scope="col">NAME</th>
+                              <th scope="col">DISTRICT</th>
+                              <th scope="col">STATE</th>
+                              <th scope="col">REGION</th>
+                              <th scope="col">BLOCK</th>
+                              <th scope="col">DELIVERY</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">${++i}</th>
+                              <td>${finalData[0].PostOffice[i]}</td>
+                            
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td colspan="2">Larry the Bird</td>
+                              <td>@twitter</td>
+                            </tr>
+                          </tbody>
+                      </table>
+                        
+                        `
+                    }
+             
+
+       })
+
 
     }
 
